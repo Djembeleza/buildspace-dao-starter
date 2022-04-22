3b85429fe832878ae2b4104155aff5776a2d3572
Djembeleza <kaola730@gmail.com>
2022-04-22 16:14:41 +0200

    3-config-nft

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to DjezisaDAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
@@ -24,6 +24,7 @@ const App = () => {
   return (
     <div className="landing">
       <h1>👀 wallet connected, now what!</h1>
+      <p>Your address: {address}</p>
     </div>);
 }
 
