41c0d7afa0bb522bbd46d73813ab612bf9da4c3f
Djembeleza <kaola730@gmail.com>
2022-04-22 17:22:07 +0200

    Created an ERC-20 token

--- scripts/3-config-nft.js
+++ scripts/3-config-nft.js
@@ -1,15 +1,15 @@
 import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
 
-const editionDrop = sdk.getEditionDrop("INSERT_EDITION_DROP_ADDRESS");
+const editionDrop = sdk.getEditionDrop("0x6823422b667091739FEEc2D7422C6B350522b620");
 
 (async () => {
   try {
     await editionDrop.createBatch([
       {
-        name: "Leaf Village Headband",
-        description: "This NFT will give you access to NarutoDAO!",
-        image: readFileSync("scripts/assets/headband.png"),
+        name: "Music Ethusiast",
+        description: "This NFT will give you access to DjezisaDAO",
+        image: readFileSync("scripts/assets/music.png"),
       },
     ]);
     console.log("✅ Successfully created a new NFT in the drop!");
